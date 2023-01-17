fb40912dcef038bff0f06d5967dbd0e21c13ea6b
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-01-17 00:28:04 -0600

    Add files via upload

--- run.py
+++ run.py
@@ -733,8 +733,8 @@ for i in range(1,6):
 
         res = stats.ttest_rel(acc_erm_cur,acc_erm)
     
-        print("overall accuracy ERM", d_utils.Average(acc_erm))
-        print("overall accuracy curriculum ERM", d_utils.Average(acc_erm_cur))
+        print("overall accuracy ERM", d_utils.Average(acc_erm), 'trials',len(acc_erm))
+        print("overall accuracy curriculum ERM", d_utils.Average(acc_erm_cur),'trials',len(acc_erm_cur))
 
         display(res)
 
