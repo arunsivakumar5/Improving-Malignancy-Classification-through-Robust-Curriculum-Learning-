120ed6b8228c479f7c06960223384ca8721e33df
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-01-18 15:27:07 -0600

    Add files via upload

--- run.py
+++ run.py
@@ -463,19 +463,18 @@ elif method =='gDRO':
             test=testDataset
 
             sampler = SequentialSampler(trainDataset)
-
             subclass_counts=trainDataset.get_class_counts('subclass')
+            train_dataloader = DataLoader(tr, batch_size =params['batch_size'],sampler=sampler,shuffle=False)
 
-            train_dataloader = DataLoader(tr, batch_size =params2['batch_size'],sampler=sampler )
-
-            val_dataloader = DataLoader(val,batch_size = len(validDataset),shuffle = False, num_workers=0)
+            sampler2 = SequentialSampler(val)
+            val_dataloader = DataLoader(val,batch_size = len(validDataset),shuffle = False,sampler=sampler2)
             test_dataloader = DataLoader(test, batch_size = len(testDataset) , shuffle = False, num_workers=0)   
 
             device = torch.device('cuda')
 
             model = models.TransferModel18()
 
-            modelA,max_acc = train_gdro(params2,model,train_dataloader,val_dataloader,num_epochs=150,mode='cur_gDRO',subclass_counts=subclass_counts)
+            modelA,max_acc = train_gdro(params,model,train_dataloader,val_dataloader,num_epochs=150,mode='cur_gDRO',subclass_counts=subclass_counts)
             modelA.load_state_dict(torch.load('.//models//Best_model_cur_gdro.pth'))
             print("Cur gDRO trained!")
 
