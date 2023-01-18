091a343dd4487bf9365c31e44b30d87e416aa76c
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-01-17 21:04:22 -0600

    Add files via upload

--- run.py
+++ run.py
@@ -491,7 +491,7 @@ elif method =='gDRO':
             device = torch.device('cuda')
             model = models.TransferModel18()
 
-            modelA,max_acc = train_gdro(params,train_dataloader,val_dataloader,model,num_epochs=150,mode ='gDRO',subclass_counts = subclass_counts)
+            modelA,max_acc = train_gdro(params,model,train_dataloader,val_dataloader,num_epochs=150,mode ='gDRO',subclass_counts = subclass_counts)
             modelA.load_state_dict(torch.load('.//models//Best_model_gdro.pth'))
             print("Traditional gDRO trained!")
 
