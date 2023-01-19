e9494c3be64eb4fe6f27ff3a3de2caedf90c2d2e
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-01-18 22:15:13 -0600

    Add files via upload

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ class TransferModel18(nn.Module):
         super(TransferModel18, self).__init__()
 
         
-        self.model = torchvision.models.resnet18(pretrained=True).to(device)
+        self.model = torchvision.models.resnet18(weights='DEFAULT').to(device)
         
 
         for param in self.model.parameters():
