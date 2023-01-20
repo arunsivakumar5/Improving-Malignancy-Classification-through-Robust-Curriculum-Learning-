25001b38627ab5a959d6009837708ebcbdd1fe9b
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-01-20 15:24:48 -0600

    Add files via upload

--- models.py
+++ models.py
@@ -12,12 +12,12 @@ class TransferModel18(nn.Module):
     def __init__(self, pretrained=True, freeze=True, device='cuda'):
         super(TransferModel18, self).__init__()
 
-        if pretrained=True:
+        if pretrained==True:
             self.model = torchvision.models.resnet18(pretrained=True).to(device)
         else:
             self.model = torchvision.models.resnet18(pretrained=False).to(device)
         
-        if freeze=True:
+        if freeze==True:
             for param in self.model.parameters():
                 param.requires_grad = False
         else:
