70ffac82b2452e9dc33306a5ba91717df714760f
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-01-24 15:05:22 -0600

    Add files via upload

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class TransferModel18(nn.Module):
     ResNet18 transfer learning model
     
     """
-    def __init__(self, pretrained=True, freeze=True, device='cuda'):
+    def __init__(self, pretrained=True, freeze=True, device='cuda',num_classes=3):
         super(TransferModel18, self).__init__()
 
         if pretrained==True:
@@ -22,10 +22,11 @@ class TransferModel18(nn.Module):
                 param.requires_grad = False
         else:
             pass
-
+        
+        self.num_classes= num_classes
         # Fully-connected layer
         self.model.fc = nn.Sequential(
-            nn.Linear(in_features=512, out_features=3, bias=True, device=device),
+            nn.Linear(in_features=512, out_features=self.num_classes, bias=True, device=device),
         ) 
         
 
