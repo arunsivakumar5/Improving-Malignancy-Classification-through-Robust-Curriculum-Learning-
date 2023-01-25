e9ee810c4a9450bc4b0a385a78e69017e362503c
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-01-25 01:21:53 -0600

    Add files via upload

--- train.py
+++ train.py
@@ -279,7 +279,7 @@ def train_gdro(params,model, train_dataloader, val_dataloader, use_cuda = True, 
 
 
 
-def train_gdro_ct(params,model, train_dataloader1, val_dataloader,train_dataloader2,val_dataloader2,  use_cuda = True, robust=True, num_epochs = 0,stable= True, size_adjustment = None,mode =None,subclass_counts1=None,subclass_counts2=None):
+def train_gdro_ct(params,model, train_dataloader1, val_dataloader1,train_dataloader2,val_dataloader2,  use_cuda = True, robust=True, num_epochs = 0,stable= True, size_adjustment = None,mode =None,subclass_counts1=None,subclass_counts2=None):
     
     
     device = torch.device("cuda")
@@ -314,15 +314,15 @@ def train_gdro_ct(params,model, train_dataloader1, val_dataloader,train_dataload
                 pass
         
     
-            while epoch>75:
+            if epoch<75:
             
                 criterion = torch.nn.CrossEntropyLoss(reduction='none')
                 criterion = LossComputer(criterion, robust,5, subclass_counts1, 0.01, stable, 12, False, size_adjustment, use_cuda= use_cuda)
                 trainloader = train_dataloader1
                 valloader = val_dataloader
 
-                break
-            if epoch ==75:
+                
+            elif epoch ==75:
             
                 valacc = 0
                 model.fc = nn.Sequential(nn.Linear(in_features=512, out_features=3, bias=True, device=device),)
