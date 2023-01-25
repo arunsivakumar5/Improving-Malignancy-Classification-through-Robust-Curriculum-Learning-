2a88ffcae28a649882b37135054c055f929eff34
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-01-24 21:39:46 -0600

    Add files via upload

--- run.py
+++ run.py
@@ -547,7 +547,7 @@ elif method =='gDRO':
                 model = models.TransferModel18(freeze=False)
 
             
-            modelA,max_acc = train_gdro_ct(params_cur,model,train_dataloader1,val_dataloader1,train_dataloader2,val_dataloader2,num_epochs=150,mode='cur_gDRO',subclass_counts1=subclass_counts1,subclass_counts2=subclass_counts2)
+            modelA,max_acc = train_gdro_ct(params_cur,model,train_dataloader1,val_dataloader1,train_dataloader2,val_dataloader2,num_epochs=300,mode='cur_gDRO',subclass_counts1=subclass_counts1,subclass_counts2=subclass_counts2)
             #modelA.load_state_dict(torch.load('.//models//Best_model_cur_gdro.pth'))
             print("Cur gDRO trained!")
 
@@ -620,7 +620,7 @@ elif method =='gDRO':
             else:
                 model = models.TransferModel18(freeze=False)
 
-            modelA,max_acc = train_gdro(params,model,train_dataloader,val_dataloader,num_epochs=150,mode ='gDRO',subclass_counts = subclass_counts)
+            modelA,max_acc = train_gdro(params,model,train_dataloader,val_dataloader,num_epochs=300,mode ='gDRO',subclass_counts = subclass_counts)
             modelA.load_state_dict(torch.load('.//models//Best_model_gdro.pth'))
             print("Traditional gDRO trained!")
 
