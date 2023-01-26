5b35dbd06b39dcde816b132873460ed5ab66d7b7
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-01-25 20:57:27 -0600

    Add files via upload

--- utils/image_data_utils.py
+++ utils/image_data_utils.py
@@ -785,7 +785,7 @@ def get_cur_features(file='./data/LIDC_3_4_Ratings_wMSE.csv',
 
     
     
-    
+        print(df_splits1)
         
         df_splits1 = df_splits1[df_splits1['noduleID'].isin(df_features['noduleID'])]
 
