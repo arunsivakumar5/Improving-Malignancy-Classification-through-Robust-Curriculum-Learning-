00150bede44aa0fc2f0d422f5d86d9ee9883c180
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-01-27 21:33:14 -0600

    Add files via upload

--- train.py
+++ train.py
@@ -364,38 +364,7 @@ def train_gdro_ct(params,model, train_dataloader1, val_dataloader1,train_dataloa
                 print("epoch", epoch,"Validation Accuracy",min(vacc1,vacc2,vacc3,vacc4))
 
 
-                if valacc > max_val_acc:
-                    max_val_acc = valacc
-                    model = cur_model
-                    old_model = model
-                    if mode=='gDRO':
-                        try:
-                            torch.save(model.state_dict(), './models/Best_model_gdro.pth')
-                            path = './models/Best_model_gdro.pth'
-                        except:
-                            os.makedirs(path)
-                    elif mode=='cur_gDRO':
-                        try:
-                            torch.save(model.state_dict(), './models/Best_model_cur_gdro.pth')
-                            path = './models/Best_model_cur_gdro.pth'
-                        except:
-                            os.makedirs(path)
-                    elif mode=='random_gDRO':
-                        try:
-                            torch.save(model.state_dict(), './models/Best_model_rand2.pth')
-                        except:
-                            pass
-                    elif mode=='Cur_gDRO':
-                        torch.save(model.state_dict(), './models/Best_model_cur2.pth')
-                    else:
-                        print("Model weights unsaved")
-                        pass
-                    perfect_epoch = epoch
-                    print("perfect epoch",perfect_epoch)
                 
-            
-                else:
-                    model = old_model
                     
                     
                 if params['scheduler_choice'] == 1:
