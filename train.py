890eeb2cd9156e793c644cbcdc57624540380897
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-01-30 10:19:14 -0600

    Add files via upload

--- train.py
+++ train.py
@@ -283,7 +283,208 @@ def train_gdro(params,model, train_dataloader, val_dataloader, use_cuda = True, 
 
     return model,max_val_acc
 
+def train_erm_ct(params,train_dataloader,val_dataloader,train_dataloader2,val_dataloader2,model,num_epochs=300,mode='cur_erm'):
+    
+    device = torch.device("cuda")
+    
+    model_new = torchvision.models.resnet18(pretrained=True).to(device)
+    
+    num_ftrs = model_new.fc.in_features
+    model_new.fc = nn.Linear(num_ftrs, 2)
+    model = model_new
+    
+    model = model.to(device)
+    
+    criterion = nn.CrossEntropyLoss()
+    
+    if params['opt'] =='Adam':
+        optimizer = torch.optim.Adam(model.parameters(), lr =params['learning_rate'],weight_decay =params['w_d']) 
+    else:
+        optimizer = torch.optim.SGD(model.parameters(), lr =params['learning_rate'],weight_decay =params['w_d']) 
+        
+    if params['scheduler_choice'] == 1:
+        scheduler =  d_utils.init_scheduler({'class_args': {'patience':params['patience'],'factor': params['factor'],'mode':'max'},'class_name': 'ReduceLROnPlateau'},optimizer) 
+    else:
+        scheduler = None
+    
+      
+    
+    max_val_acc = -1
+    for epoch in range(num_epochs):
+        
+        
+            if params['scheduler_choice'] == 1:
+                scheduler.last_epoch = epoch - 1
+            else:
+                pass
+        
+    
+            
+            
+            
+            model.train()
+            if epoch <75:
+                model.train()
+                for train_input,train_label in trainDataloader1:
 
+
+                    train_label = train_label['superclass']
+                
+                    train_label = train_label.to(device)
+                    train_input = train_input.to(device)
+                
+                    optimizer.zero_grad()
+                    output = model(train_input)
+                
+                    _, predictions = output.max(1)
+      
+                    loss = criterion(output,train_label)
+                    batch_loss =loss
+                
+
+                
+                
+                    model.zero_grad()
+                
+                    batch_loss.backward()
+                    optimizer.step()
+                
+            
+                   
+                    
+                model.eval()
+                cur_model = model
+            
+                with torch.no_grad():
+
+                        acc,a1,a2,a3,a4,a5 = d_utils.evaluate(validDataloader,model,5,verbose = True)
+                        if scheduler:
+                            scheduler.step(acc) 
+                        else:
+                            pass
+                    
+                        print("acc",acc)
+                        print("Max acc",max_val_acc)
+                        if acc > max_val_acc:
+                            max_val_acc =acc
+                            model = cur_model
+                            old_model = model
+                            if mode=='erm':
+                                torch.save(model.state_dict(), './models/Best_model_erm.pth')
+                            elif mode=='cur_erm':
+                                torch.save(model.state_dict(), './models/Best_model_cur_erm.pth')
+                            elif mode=='random_feature_ext':
+                                torch.save(model.state_dict(), './models/Best_model_rand1.pth')
+                            elif mode=='Cur_feature_ext':
+                                torch.save(model.state_dict(), './models/Best_model_cur1.pth')
+                            else:
+                                print("Model weights unsaved")
+                                pass
+                            perfect_epoch = epoch_num
+                            print("perfect epoch",perfect_epoch)
+                        else:
+                            try:
+                                model = old_model
+                            except:
+                                old_model = model
+
+
+            else:
+                for train_input,train_label in trainDataloader2:
+
+
+                    train_label = train_label['superclass']
+                
+                    train_label = train_label.to(device)
+                    train_input = train_input.to(device)
+                
+                    optimizer.zero_grad()
+                    if epoch==75:
+                        model_new = model
+                        num_ftrs = model_new.fc.in_features
+                        model_new.fc = nn.Linear(num_ftrs, 3)
+                        model = model_new
+                        model = model.to(device)   
+                        max_val_acc =-1
+                    else:
+                        pass
+                    output = model(train_input)
+                
+                    _, predictions = output.max(1)
+      
+                    loss = criterion(output,train_label)
+                    batch_loss =loss
+                
+
+                
+                
+                    model.zero_grad()
+                
+                    batch_loss.backward()
+                    optimizer.step()
+                
+            
+                   
+                    
+                model.eval()
+                cur_model = model
+            
+                with torch.no_grad():
+
+                        acc,a1,a2,a3,a4,a5 = d_utils.evaluate(validDataloader,model,5,verbose = True)
+                        if scheduler:
+                            scheduler.step(acc) 
+                        else:
+                            pass
+                    
+                        print("acc",acc)
+                        print("Max acc",max_val_acc)
+                        if acc > max_val_acc:
+                            max_val_acc =acc
+                            model = cur_model
+                            old_model = model
+                            if mode=='erm':
+                                torch.save(model.state_dict(), './models/Best_model_erm.pth')
+                            elif mode=='cur_erm':
+                                torch.save(model.state_dict(), './models/Best_model_cur_erm.pth')
+                            elif mode=='random_feature_ext':
+                                torch.save(model.state_dict(), './models/Best_model_rand1.pth')
+                            elif mode=='Cur_feature_ext':
+                                torch.save(model.state_dict(), './models/Best_model_cur1.pth')
+                            else:
+                                print("Model weights unsaved")
+                                pass
+                            perfect_epoch = epoch_num
+                            print("perfect epoch",perfect_epoch)
+                        else:
+                            try:
+                                model = old_model
+                            except:
+                                old_model = model
+                    
+ 
+    
+                    
+            
+        
+        
+            
+            
+        
+            
+            
+                
+                    
+                    
+                if params['scheduler_choice'] == 1:
+                    scheduler.step(valacc)
+                else:
+                    pass
+                
+                
+                
+     
+    return model,max_val_acc
 
 
 def train_gdro_ct(params,model, train_dataloader1, val_dataloader1,train_dataloader2,val_dataloader2,num_epochs = 0,mode =None, subclass_counts1=None,subclass_counts2=None, use_cuda = True, robust=True, stable= True, size_adjustment = None):
