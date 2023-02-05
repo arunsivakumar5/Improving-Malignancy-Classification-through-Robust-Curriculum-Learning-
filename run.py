4442396dfc8b6a8b27b91ce9d3aefda843636e94
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-02-04 22:50:26 -0600

    Add files via upload

--- run.py
+++ run.py
@@ -699,7 +699,7 @@ elif method =='gDRO':
             steps1 = math.ceil(len(trainDataset1) / params['batch_size'])
             steps2 = math.ceil(len(trainDataset2) / params['batch_size'])
 
-            modelA,max_acc = train_gdro_ct_new(params,model,train_dataloader1,val_dataloader1,train_dataloader2,val_dataloader2,num_epochs=300,mode='cur_gDRO',subclass_counts1=subclass_counts1,subclass_counts2=subclass_counts2,steps1=steps1,steps2=steps2)
+            modelA,max_acc = train_gdro_ct_new(params,model,train_dataloader1,val_dataloader1,train_dataloader2,val_dataloader2,num_epochs=100,mode='cur_gDRO',subclass_counts1=subclass_counts1,subclass_counts2=subclass_counts2,steps1=steps1,steps2=steps2)
             modelA.load_state_dict(torch.load('.//models//Best_model_cur_gdro.pth'))
             print("Cur gDRO trained!")
 
@@ -771,7 +771,7 @@ elif method =='gDRO':
             steps = math.ceil(len(trainDataset) / params['batch_size'])
             
 
-            modelA,max_acc = train_gdro_new(params,model,train_dataloader,val_dataloader,num_epochs=300,mode ='gDRO',subclass_counts = subclass_counts,steps=steps)
+            modelA,max_acc = train_gdro_new(params,model,train_dataloader,val_dataloader,num_epochs=100,mode ='gDRO',subclass_counts = subclass_counts,steps=steps)
             modelA.load_state_dict(torch.load('.//models//Best_model_gdro.pth'))
             print("Traditional gDRO trained!")
 
