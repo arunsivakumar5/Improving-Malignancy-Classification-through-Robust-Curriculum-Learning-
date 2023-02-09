6a38c87ca40a7fc81c13bc65907034245a2d7d60
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-02-08 18:11:13 -0600

    Add files via upload

--- run.py
+++ run.py
@@ -202,7 +202,7 @@ if method =='random':
 else:
     pass
 
-
+OUTDATED_IGNORE=1 
 
 
 
@@ -440,7 +440,7 @@ elif method =='gDRO':
 
             
             train_dataloader1 = DataLoader(tr, batch_size =params['batch_size'],shuffle=False,sampler=torch.utils.data.WeightedRandomSampler(train_weights1,len(train_weights1)) )
-            #train_dataloader1 = DataLoader(tr, batch_size =params['batch_size'],sampler=SequentialSampler(trainDataset1),shuffle=False)
+            
 
             try:
                 val_weights1 =   im_utils.get_sampler_weights(validDataset1.subclasses)
@@ -460,7 +460,7 @@ elif method =='gDRO':
 
             
             train_dataloader2 = DataLoader(tr, batch_size =params['batch_size'],shuffle=False,sampler=torch.utils.data.WeightedRandomSampler(train_weights2,len(train_weights2)) )
-            #train_dataloader2 = DataLoader(tr, batch_size =params['batch_size'],sampler=SequentialSampler(trainDataset2),shuffle=False)
+            
 
             val_weights2 =   im_utils.get_sampler_weights(validDataset2.subclasses)
             val_dataloader2 = DataLoader(val,batch_size = len(validDataset2) ,shuffle = False,sampler = torch.utils.data.WeightedRandomSampler(val_weights2,len(val_weights2)) )
