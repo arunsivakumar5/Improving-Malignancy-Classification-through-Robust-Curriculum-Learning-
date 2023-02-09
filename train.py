9a8138b994aa8ebf435e50468cf5dce9529ece44
Arun Sivakumar <86889886+arunsivakumar5@users.noreply.github.com>
2023-02-08 19:26:40 -0600

    Add files via upload

--- train.py
+++ train.py
@@ -937,7 +937,9 @@ def train_gdro_ct_new(params,model, train_dataloader1, val_dataloader1,train_dat
                     overall_val_accs_lst2.append(over_val_acc)
                     train_accs_lst.append(epochs)
                     val_accs_lst.append(valacc)
-                    print("epoch", epochs,"Worst Group Validation Accuracy",min(vacc1,vacc2,vacc3,vacc4,v5),"Overall Validation Accuracy",over_val_acc)
+                    print("epoch", epochs,'\n'
+                          "Worst Group Validation Accuracy",min(vacc1,vacc2,vacc3,vacc4,v5),'\n'
+                          "Overall Validation Accuracy",over_val_acc)
                 else:
                     pass
 
@@ -1006,7 +1008,9 @@ def train_gdro_ct_new(params,model, train_dataloader1, val_dataloader1,train_dat
                     overall_val_accs_lst2.append(over_val_acc)
                     train_accs_lst.append(epochs)
                     val_accs_lst.append(valacc)
-                    print("epoch", epochs,"Worst Group Validation Accuracy",min(vacc1,vacc2,vacc3,vacc4,v5),"Overall Validation Accuracy",over_val_acc)
+                    print("epoch", epochs,'\n'
+                          "Worst Group Validation Accuracy",min(vacc1,vacc2,vacc3,vacc4,v5),'\n'
+                          "Overall Validation Accuracy",over_val_acc)
                     
                 else:
                     pass
